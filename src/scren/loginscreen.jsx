000b86907009c1b6ca1885479539200ebe093010
rka28 <karim.bara@epitech.eu>
2024-06-03 16:09:18 +0200

    add:css login

--- src/scren/loginscreen.jsx
+++ src/scren/loginscreen.jsx
@@ -1,51 +1,89 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
 import React from 'react';
-import FontAwesome from 'react-native-vector-icons/dist/FontAwesome';
-
+import FontAwesome from "react-native-vector-icons/dist/FontAwesome";
 
 const loginscreen = () => {
-    return (
-        <View style={styles.container}>
-            <View style={styles.TopimageContainer}>
-                <Image source={require("../asset/logintop.png")} style={styles.topImage} />
-            </View>
+	return (
+		<View style={styles.container}>
+			<View style={styles.TopimageContainer}>
+				<Image source={require("../asset/logintop.png")} style={styles.topImage} />
+			</View>
 			<View style={styles.helloContainer}>
 				<Text style={styles.helloText}>Hello</Text>
 			</View>
 			<View>
-				<Text style={styles.singIntext}>Sign in to your account</Text>
+				<Text style={styles.signIntext}>Sign in to your account</Text>
 			</View>
-			<FontAwesome name={"user"} size={30} color={"black"}/>
-        </View>
-    );
+			<View style={styles.inputContainer}>
+				<FontAwesome 
+				name={"user"} 
+				size={30} 
+				color={"#C8C8C8"} 
+				style={styles.inputIcon}/>
+			
+				<TextInput style={styles.textInput} placeholder="Email" />
+
+			</View>
+			<View style={styles.inputContainer}>
+				<FontAwesome 
+				name={"lock"} 
+				size={30} 
+				color={"#C8C8C8"} 
+				style={styles.inputIcon}/>
+			
+				<TextInput style={styles.textInput} placeholder="Password" secureTextEntry />
+
+			</View>
+		</View>
+	);
 };
 
 export default loginscreen;
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: "#F5F5F5",
-        flex: 1,
-    },
-    TopimageContainer: {
-      
-    },
-	topImage:{
+	container: {
+		backgroundColor: "#F5F5F5",
+		flex: 1,
+	},
+	TopimageContainer: {
+
+	},
+	topImage: {
 		width: "100%",
-		height:120,
+		height: 120,
 	},
-	helloContainer:{
-		
+	helloContainer: {
+
 	},
-	helloText:{
-		textAlign:"center",
+	helloText: {
+		textAlign: "center",
 		fontSize: 70,
-		fontWeight:"500",
-		color:"#262626",
+		fontWeight: "500",
+		color: "#262626",
 	},
-	singIntext: {
-		textAlign:"center",
+	signIntext: {
+		textAlign: "center",
 		fontSize: 18,
-		color:"#262626",
+		color: "#262626",
+		marginBottom: 20,
+	},
+	inputContainer: {
+		backgroundColor: "#FFFFFF",
+		flexDirection: "row",
+		borderRadius: 10,
+		marginHorizontal: 40,
+		elevation: 10,
+		marginVertical: 20,
+		alignItems: "center",
+		height: 70,
+
+	},
+	inputIcon: {
+		marginLeft: 15,
+	},
+	textInput: {
+		flex: 1,
+		paddingLeft: 10,
 	}
+
 });
