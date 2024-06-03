f9d6ae0d0e8f593b68e5d9efb4f2104171373c78
rka28 <karim.bara@epitech.eu>
2024-06-03 17:21:29 +0200

    add:docs

--- src/scren/loginscreen.jsx
+++ src/scren/loginscreen.jsx
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
 import React from 'react';
 import FontAwesome from "react-native-vector-icons/dist/FontAwesome";
+import LinearGradient from 'react-native-linear-gradient';
 
 const loginscreen = () => {
 	return (
@@ -15,24 +16,33 @@ const loginscreen = () => {
 				<Text style={styles.signIntext}>Sign in to your account</Text>
 			</View>
 			<View style={styles.inputContainer}>
-				<FontAwesome 
-				name={"user"} 
-				size={30} 
-				color={"#C8C8C8"} 
-				style={styles.inputIcon}/>
-			
+				<FontAwesome
+					name={"user"}
+					size={30}
+					color={"#C8C8C8"}
+					style={styles.inputIcon} />
+
 				<TextInput style={styles.textInput} placeholder="Email" />
 
 			</View>
 			<View style={styles.inputContainer}>
-				<FontAwesome 
-				name={"lock"} 
-				size={30} 
-				color={"#C8C8C8"} 
-				style={styles.inputIcon}/>
-			
+				<FontAwesome
+					name={"lock"}
+					size={30}
+					color={"#C8C8C8"}
+					style={styles.inputIcon} />
+
 				<TextInput style={styles.textInput} placeholder="Password" secureTextEntry />
 
+			</View>
+			<Text style={styles.forgotPasswordtext}>Forgot your password?</Text>
+			<View style={styles.singInButtonContainer}>
+				<Text style={styles.singIn}>Sign in</Text>
+				<LinearGradient colors={['#4c669f', '#3b5998', '#192f6a']} style={styles.linearGradient}>
+					<Text style={styles.buttonText}>
+						Sign in with Facebook
+					</Text>
+				</LinearGradient>
 			</View>
 		</View>
 	);
@@ -84,6 +94,21 @@ const styles = StyleSheet.create({
 	textInput: {
 		flex: 1,
 		paddingLeft: 10,
-	}
+	},
+	forgotPasswordtext: {
+		color: "#BEBEBE",
+		textAlign: "right",
+		width: "90%",
+		fontsize: 15,
+	},
+	singInButtonContainer: {
+
+	},
+	singIn: {
+		color: "#262626",
+		fontSize: 25,
+		fontWeight: "blod",
+
+	},
 
 });
