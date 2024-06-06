0f01edadc0f2a7ea78916d7026db403bd01f923e
L0ouk <loukas.carrasco-bustos@epitech.eu>
2024-06-06 11:51:46 +0200

    ajout de chgangment pp user

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,17 +2,19 @@ import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from '../src/scren/loginscreen';
-import SingupScreen from '../src/scren/singupscreen';
+// import LoginScreen from '../src/scren/loginscreen';
+// import SingupScreen from '../src/scren/singupscreen';
 
+import profile from '../src/screen/userpp';
 const Stack = createNativeStackNavigator();
 
 const _layout = () => {
   return (
     <NavigationContainer independent={true}>
       <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Singup" component={SingupScreen} />
+        {/* <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Singup" component={SingupScreen} /> */}
+        <Stack.Screen name="profile" component={profile} />
       </Stack.Navigator>
     </NavigationContainer>
   );
