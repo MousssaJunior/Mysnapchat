cf8a6cd8ca2eafea463ec09101e9467ff07400f5
rka28 <karim.bara@epitech.eu>
2024-06-15 17:07:30 +0200

    add:username on snap

--- src/scren/Recivedreponse.tsx
+++ src/scren/Recivedreponse.tsx
@@ -1,69 +1,131 @@
+
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, ScrollView, Image, ActivityIndicator, Alert } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, ActivityIndicator, Alert, TouchableOpacity } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 import { useRoute } from '@react-navigation/native';
 
 const ReceivedSnaps = () => {
-  const [snaps, setSnaps] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const route = useRoute();
-  const { userId } = route.params || "";
+  const [snaps, setSnaps] = useState([]); // État pour stocker les snaps reçus
+  const [loading, setLoading] = useState(true); // État pour le chargement
+  const [username, setUsername] = useState(''); // État pour stocker le nom d'utilisateur
+  const [selectedSnap, setSelectedSnap] = useState(null); // État pour stocker le snap sélectionné
+  const route = useRoute(); // Obtenir les paramètres de la route actuelle
+  const { userId } = route.params || ''; // Identifiant de l'utilisateur récupéré depuis les paramètres de la route
 
   useEffect(() => {
     const fetchSnaps = async () => {
-      const token = await AsyncStorage.getItem('token');
-      console.log(token);
+      const token = await AsyncStorage.getItem('token'); // Récupérer le token d'authentification depuis AsyncStorage
 
       try {
-        console.log('Fetching snap list...');
-        const response = await axios.get('https://snapchat.epidoc.eu/snap', {
+        // Récupération des détails de l'utilisateur
+        const userResponse = await axios.get('https://snapchat.epidoc.eu/user', {
           headers: {
             'Content-Type': 'application/json',
             'Authorization': 'Bearer ' + token,
-            "x-api-key": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImthcmltLmJhcmFAZXBpdGVjaC5ldSIsImlhdCI6MTcxODEwNjgzOH0.8E6eoi_eRSd7TLYUG3p2BMtTfiQxzzVf25mStXIqJq0",
+            'x-api-key': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1vdXNzYS1qdW5pb3IuZm9mYW5hQGVwaXRlY2guZXUiLCJpYXQiOjE3MTgwMTEwNTh9.hI23vvbPZcA1cZDm5cYkgydL2cHn3tO2DGHLhQgvFCI',
           },
         });
 
-        if (response.status !== 200) {
-          throw new Error(`Erreur HTTP ! statut : ${response.status}`);
+        if (userResponse.status !== 200) {
+          throw new Error(`Erreur HTTP ! statut : ${userResponse.status}`);
         }
 
-        const snapList = response.data.data || [];
+        const userData = userResponse.data.data;
+        const fetchedUsername = userData.username;
+        setUsername(fetchedUsername); // Mettre à jour le nom d'utilisateur dans l'état
 
+        // Récupération des snaps de l'utilisateur
+        const snapResponse = await axios.get('https://snapchat.epidoc.eu/snap', {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token,
+            'x-api-key': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1vdXNzYS1qdW5pb3IuZm9mYW5hQGVwaXRlY2guZXUiLCJpYXQiOjE3MTgwMTEwNTh9.hI23vvbPZcA1cZDm5cYkgydL2cHn3tO2DGHLhQgvFCI',
+          },
+        });
+
+        if (snapResponse.status !== 200) {
+          throw new Error(`Erreur HTTP ! statut : ${snapResponse.status}`);
+        }
+
+        const snapList = snapResponse.data.data || []; // Liste des snaps récupérée depuis la réponse
+
+        // Récupérer les détails de chaque snap avec une promesse asynchrone
         const snapDetailsPromises = snapList.map(async (snap) => {
-          const snapResponse = await axios.get(`https://snapchat.epidoc.eu/snap/${snap._id}`, {
+          // Récupération des détails de chaque snap
+          const snapDetailResponse = await axios.get(`https://snapchat.epidoc.eu/snap/${snap._id}`, {
             headers: {
               'Content-Type': 'application/json',
               'Authorization': 'Bearer ' + token,
-              "x-api-key": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImthcmltLmJhcmFAZXBpdGVjaC5ldSIsImlhdCI6MTcxODEwNjgzOH0.8E6eoi_eRSd7TLYUG3p2BMtTfiQxzzVf25mStXIqJq0",
+              'x-api-key': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1vdXNzYS1qdW5pb3IuZm9mYW5hQGVwaXRlY2guZXUiLCJpYXQiOjE3MTgwMTEwNTh9.hI23vvbPZcA1cZDm5cYkgydL2cHn3tO2DGHLhQgvFCI',
             },
           });
 
-          return snapResponse.data;
+          // Récupérer les détails de l'utilisateur qui a envoyé le snap (sender)
+          const senderResponse = await axios.get(`https://snapchat.epidoc.eu/user/${snap.from}`, {
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': 'Bearer ' + token,
+              'x-api-key': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1vdXNzYS1qdW5pb3IuZm9mYW5hQGVwaXRlY2guZXUiLCJpYXQiOjE3MTgwMTEwNTh9.hI23vvbPZcA1cZDm5cYkgydL2cHn3tO2DGHLhQgvFCI',
+            },
+          });
+
+          // Créer un nouvel objet snap avec les informations nécessaires
+          const snapData = {
+            _id: snap._id,
+            from: snap.from,
+            senderUsername: senderResponse.data.data.username,
+            date: snap.date,
+            duration: snapDetailResponse.data.data.duration,
+            image: snapDetailResponse.data.data.image,
+          };
+
+          return snapData; // Retourner les données du snap avec le nom d'utilisateur du sender et les détails du snap
         });
 
-        const snapDetails = await Promise.all(snapDetailsPromises);
-        setSnaps(snapDetails);
+        const snapDetails = await Promise.all(snapDetailsPromises); // Attendre que toutes les promesses soient résolues
+        setSnaps(snapDetails); // Mettre à jour les snaps dans l'état
+
       } catch (error) {
-        console.log('Réponse d\'erreur :', error.response ? error.response.data : error.message);
+        console.log('Erreur :', error.response ? error.response.data : error.message);
         let errorMessage = 'Une erreur est survenue.';
         if (error.response && error.response.data.message) {
           errorMessage = error.response.data.message;
         }
-        Alert.alert('Erreur', errorMessage);
+        Alert.alert('Erreur', errorMessage); // Afficher une alerte en cas d'erreur
       } finally {
-        setLoading(false);
+        setLoading(false); // Marquer le chargement comme terminé
       }
     };
 
-    fetchSnaps();
-  }, [userId]);
+    fetchSnaps(); // Appeler la fonction pour récupérer les snaps lors du montage du composant
+  }, [userId]); // Effectuer la requête à chaque changement de userId
+
+  const handleSnapSelect = (snap) => {
+    setSelectedSnap(snap); // Mettre à jour l'état du snap sélectionné
+  };
+
+  const handleBackToSnaps = () => {
+    setSelectedSnap(null); // Réinitialiser l'état du snap sélectionné pour revenir à la liste des snaps
+  };
+
+  const renderSnapDetails = () => (
+    <View key={selectedSnap._id} style={styles.selectedSnapContainer}>
+      <Text style={styles.sender}>De: {selectedSnap.senderUsername}</Text>
+      <Text>Date: {new Date(selectedSnap.date).toLocaleString()}</Text>
+      <Text>Durée: {selectedSnap.duration} secondes</Text>
+      {selectedSnap.image ? (
+        <Image source={{ uri: selectedSnap.image }} style={styles.snapImage} />
+      ) : (
+        <Text>Image indisponible</Text>
+      )}
+    </View>
+  );
 
   if (loading) {
     return (
       <View style={styles.container}>
-        <Text>En cours...</Text>
+        <Text>Chargement...</Text>
         <ActivityIndicator size="large" color="#0000ff" />
       </View>
     );
@@ -72,22 +134,34 @@ const ReceivedSnaps = () => {
   if (snaps.length === 0) {
     return (
       <View style={styles.container}>
-        <Text>Vous n'avez pas de snaps</Text>
+        <Text>Aucun snap disponible</Text>
       </View>
     );
   }
 
   return (
+
     <ScrollView style={styles.container}>
-      <Text style={styles.header}>SNAPS REÇUS</Text>
-      {snaps.map((snap) => (
-        <View key={snap.data._id} style={styles.snapContainer}>
-          <Text style={styles.sender}>De: {snap.data.from}</Text>
-          <Text>Date: {new Date(snap.data.date).toLocaleString()}</Text>
-          <Text>Durée: {snap.data.duration} secondes</Text>
-          <Image source={{ uri: snap.data.image }} style={styles.snapImage} />
+      {selectedSnap ? (
+        <View>
+          <TouchableOpacity onPress={handleBackToSnaps} style={styles.backButton}>
+            <Text style={styles.backButtonText}>Retour aux snaps</Text>
+          </TouchableOpacity>
+          {renderSnapDetails()}
         </View>
-      ))}
+      ) : (
+        <View>
+          <Text style={styles.header}>SNAPS REÇUS pour {username}</Text>
+          {snaps.map((snap) => (
+            <TouchableOpacity key={snap._id} onPress={() => handleSnapSelect(snap)}>
+              <View style={styles.snapContainer}>
+                <Text style={styles.sender}>De: {snap.senderUsername}</Text>
+                <Text>Date: {new Date(snap.date).toLocaleString()}</Text>
+              </View>
+            </TouchableOpacity>
+          ))}
+        </View>
+      )}
     </ScrollView>
   );
 };
@@ -97,6 +171,7 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 16,
     backgroundColor: '#fff',
+
   },
   header: {
     fontSize: 24,
@@ -113,9 +188,40 @@ const styles = StyleSheet.create({
     shadowRadius: 4,
     shadowOffset: { width: 0, height: 2 },
   },
+  selectedSnapContainer: {
+    marginBottom: 16,
+    padding: 16,
+    backgroundColor: '#f9f9f9',
+    borderRadius: 8,
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    shadowOffset: { width: 0, height: 2 },
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+  },
   sender: {
     fontSize: 18,
     fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  backButton: {
+    padding: 10,
+    backgroundColor: '#DDDDDD',
+    marginBottom: 10,
+    borderRadius: 8,
+    alignItems: 'center',
+  },
+  backButtonText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  selectedSnapImage: {
+    marginTop: 10,
+    width: '100%',
+    height: 200,
+    resizeMode: 'cover',
+    borderRadius: 8,
   },
   snapImage: {
     marginTop: 10,
